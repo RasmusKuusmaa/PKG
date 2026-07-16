8a71721d352a50ee4c07bcd6c7227dce317fb450
77f315 <77f315@gmail.com>
2026-07-16 03:20:11 +0300

    spikes

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,22 +4,40 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var tilemap = $"../TileMapLayer"
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("ui_left", "ui_right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	check_deadly_tile()
+
+
+func respawn():
+	print("fe")
+	global_position = $"../SpawnPoint".global_position
+
+
+func check_deadly_tile():
+
+	var check_position = global_position + Vector2(0, 50)
+	var cell = tilemap.local_to_map(tilemap.to_local(check_position))
+	var data = tilemap.get_cell_tile_data(cell)
+	if data and data.get_custom_data("deadly") == true:
+		respawn()
