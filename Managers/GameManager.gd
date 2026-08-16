446e51879154841f4b15255c625b884adc624437
Rasmus Kuusmaa <77f315@gmail.com>
2026-08-16 23:22:48 +0300

    unclocked levels system

--- Managers/GameManager.gd
+++ Managers/GameManager.gd
@@ -1,10 +1,16 @@
 extends Node
 
 var completed_levels: Dictionary = {}
+var unlocked_lvl = 1
 
 func complete_level(level_id: String, level_time):
 	completed_levels[level_id] = level_time	
+	increase_unlocked_lvl(level_id)
 
 func get_level_time(level_id: String):
 	if completed_levels.has(level_id):
 		return completed_levels[level_id]
+
+func increase_unlocked_lvl(level_id):
+	var level_number = int(level_id.trim_prefix("level_"))
+	unlocked_lvl = max(level_number + 1, unlocked_lvl)
--- Scripts/level_selector.gd
+++ Scripts/level_selector.gd
@@ -8,13 +8,17 @@ func _ready() -> void:
 func update_level_buttons() -> void:
 	var lvl1 = GM.get_level_time("level_1")
 	var lvl2 = GM.get_level_time("level_2")
-
+	var buttons = $GridContainer.get_children()
+	for i in range(buttons.size()):
+		var button = buttons[i]
+		button.disabled = i + 1 > GM.unlocked_lvl	
+		
 	if lvl1 != null:
 		$GridContainer/Button.text = "Level 1 - %.2f seconds" % lvl1
 
 	if lvl2 != null:
 		$GridContainer/Button2.text = "Level 2 - %.2f seconds" % lvl2
-
+	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
