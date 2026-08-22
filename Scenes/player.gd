e858ea4f25a95278187c5f05a6548fe8282aab66
Rasmus Kuusmaa <77f315@gmail.com>
2026-08-22 23:08:23 +0300

    coyote timer

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -5,18 +5,24 @@ const SPEED = 300.0
 const ACCELERATION = 1800.0
 const FRICTION = 1400.0
 const JUMP_VELOCITY = -400.0
+const COYOTE_TIME = 0.12
 
+var coyote_timer := 0.0
 @onready var tilemap = $"../TileMapLayer"
 
 
 func _physics_process(delta: float) -> void:
 	# Add gravity
-	if not is_on_floor():
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+	else:
+		coyote_timer -= delta
 		velocity += get_gravity() * delta
-
+	
 	# Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and coyote_timer > 0:
 		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0
 
 	# Movement
 	var direction := Input.get_axis("ui_left", "ui_right")
